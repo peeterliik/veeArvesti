ccc9503d827b2f923694f28a83c5f848c92d2fa3
Janeli <6unli@users.noreply.github.com>
2021-03-25 18:18:01 +0200

    Update Tesseract.java

--- src/Tesseract.java
+++ src/Tesseract.java
@@ -5,8 +5,8 @@ public class Tesseract {
     public static int tuvastus(String failinimi) throws Exception {
 
         int vesi;
-        net.sourceforge.tess4j.Tesseract tesseract=new net.sourceforge.tess4j.Tesseract();
-        tesseract.setDatapath("tessdata-master");
+        net.sourceforge.tess4j.Tesseract tesseract = new net.sourceforge.tess4j.Tesseract();
+        tesseract.setDatapath("tessdata-master"); // Masinõppe andmed
         tesseract.setTessVariable("user_defined_dpi","96");
         vesi=Integer.parseInt(tesseract.doOCR(new File(failinimi)));
         return vesi;
